0dd1ee7a5fbaae60acbbd457e4de35655ed6fac3
Julia Bednaia <juliabednaia@gmail.com>
2023-07-20 06:40:42 +0300

    changed text page 404

--- react-burger/src/pages/not-found-404/not-found-404.tsx
+++ react-burger/src/pages/not-found-404/not-found-404.tsx
@@ -6,7 +6,7 @@ export function Page404() {
         <div className={`${styles.pageWrapper}`}>
             <div className={styles.textWrapper}>
                 <div className={`text text_type_digits-large ${styles.neonText} ${styles.textShaddow}`}>404</div>
-                <p className={`text text_type_main-small mt-4 ${styles.neonText} ${styles.textShaddow}`}>такой страницы не существует</p>
+                <p className={`text text_type_main-small mt-4 ${styles.neonText} ${styles.textShaddow}`}>Такой страницы нет</p>
             </div>
             <Link to="/" className={styles.link}>
                 <p className={`text text_type_main-default mt-4 mt-10`}>Перейти на главную страницу</p>
